36e7862a85886bd690c8a926431cc26ed7f7f110
Nick <nick.strassburger@smail.emt.h-brs.de>
2023-12-10 14:33:43 +0100

    Add queen movement patterns

--- Board.java
+++ Board.java
@@ -14,8 +14,8 @@ public class Board {
     public Board() {
         pieces.add(new Rook(WHITE, new C(0, 0), this));
         pieces.add(new Knight(WHITE, new C(0, 1), this));
-        pieces.add(new Bishop(WHITE, new C(3, 2), this));
-        pieces.add(new Queen(WHITE, new C(0, 3), this));
+        pieces.add(new Bishop(WHITE, new C(0, 2), this));
+        pieces.add(new Queen(WHITE, new C(3, 2), this));
         pieces.add(new King(WHITE, new C(0, 4), this));
         pieces.add(new Bishop(WHITE, new C(0, 5), this));
         pieces.add(new Knight(WHITE, new C(0, 6), this));
--- Queen.java
+++ Queen.java
@@ -1,6 +1,233 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queen extends Piece{
     public Queen(boolean white, C pos, Board board) {
         super(white, pos, board);
+    }
+
+    public List<C> getPossibleMoves(Board board) {
+        List<C> moves = new ArrayList<C>();
+        C toAdd;
+        boolean reachedObstacle ;
+
+        // +y -> right side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(0, 1);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getY() > 7) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // -y -> left side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(0, -1);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getY() < 0) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // +x -> bottom side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(1, 0);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getX() > 7) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // -x -> top side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(-1, 0);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getX() < 0) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // +y +x -> right bottom side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(1, 1);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getY() > 7 || toAdd.getX() > 7) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // -y +x -> left bottom side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(1, -1);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getY() < 0 || toAdd.getX() > 7) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // +y -x -> right top side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(-1, 1);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getY() > 7 || toAdd.getX() < 0) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        // -y -x -> left top side
+        toAdd = getPos();
+        reachedObstacle = false;
+        while (!reachedObstacle) {
+            toAdd = new C(toAdd).addr(-1, -1);
+
+            // Check other pieces in way
+            for (Piece piece : board.getPieces()) {
+                if (piece.getPos().isSame(toAdd)) {
+                    if (piece.isWhite() != isWhite()) {
+                        moves.add(toAdd);
+                    }
+                    reachedObstacle = true;
+                }
+            }
+
+            // Check out of bounds
+            if (toAdd.getY() < 0 || toAdd.getX() < 0) {
+                reachedObstacle = true;
+            }
+
+            // Add Move
+            if (!reachedObstacle) {
+                moves.add(toAdd);
+            }
+        }
+
+        return moves;
     }
 
     public char getChar() {
